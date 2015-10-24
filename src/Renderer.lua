38504f0ef06e482889d5fe6e265f3b0312ed6e71
Dustin Shaffer <dustin.n.shaffer@gmail.com>
2015-10-23 21:45:13 -0700

    - Rendering spike. Now orients player ship in direction of vel.  Player always centered. Also print loc and vel info. (Modified scale to increase readability)

--- src/Renderer.lua
+++ src/Renderer.lua
@@ -1,26 +1,29 @@
 local Class  = require('hump.class')
 local Camera = require('hump.camera')
+local Vector = require('hump.vector')
 
+local UP_VECTOR = Vector(0, 1)
+local function getAngle(xVec)
+  return math.pi - UP_VECTOR:angleTo(xVec)
+end
 
 local Renderer = Class {}
 
 function Renderer:init(world)
   self.camera = Camera()
   self.world = world
-  self.camera.scale = 1 / 10
+  self.camera.scale = 1 / 3
   
   self.playerShip = love.graphics.newImage("assets/ship.png")
 
   self.camera:lookAt(0, 0)  
 end
 
-function Renderer:drawOnCenter(image, loc, rotation)
+function Renderer:drawOnCenter(image, rotation)
   love.graphics.push()
-  love.graphics.translate(0, 0)
   love.graphics.rotate(rotation)
-  love.graphics.translate(loc.x - image:getWidth() / 2, loc.y - image:getHeight() / 2)
-  
-  love.graphics.draw(image,0, 0)
+  love.graphics.translate(-image:getWidth() / 2, -image:getHeight() / 2)
+  love.graphics.draw(image, 0, 0)  
   love.graphics.pop()  
 end
 
@@ -28,23 +31,24 @@ function Renderer:draw()
   local screenWidth  = love.graphics.getWidth()
   local screenHeight = love.graphics.getHeight()
   
-  local playerLoc = self.world.player.loc
-
   self.camera:attach()
   
   love.graphics.setColor(255,255,255)
   
+  local playerVel = self.world.player.vel
+  local playerAngle = getAngle(playerVel)
 
+  self:drawOnCenter(self.playerShip, playerAngle)
   
-  love.graphics.setColor(255,255,255)
+  love.graphics.setColor(255, 0, 0)
+  love.graphics.circle("fill", 0, 0, 10, 50)
   
-  self:drawOnCenter(self.playerShip, playerLoc,0)
-  
-  love.graphics.setColor(255,0,0)
-  love.graphics.circle("fill",playerLoc.x, playerLoc.y, 10, 50)
+  local loc = 'LOC:['.. math.floor(self.world.player.loc.x) .. ', ' .. math.floor(self.world.player.loc.y) .. ']'
+  love.graphics.print(loc, 50, 50, 0, 5, 5)
+  local vel = 'VEL:['.. math.floor(self.world.player.vel.x) .. ', ' .. math.floor(self.world.player.vel.y) .. ']'
+  love.graphics.print(vel, 50, 100, 0, 5, 5)
   
   self.camera:detach()  
-  
 end
 
 return Renderer
