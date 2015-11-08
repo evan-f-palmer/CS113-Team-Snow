ed672bb76c53bb34d29804643114519e5f0c2df3
Dustin Shaffer <dustin.n.shaffer@gmail.com>
2015-11-07 16:09:23 -0800

    - Renderer: Now draws in layers, by object "type" field, in the order specified in "DRAW_ORDERING"

--- src/Renderer.lua
+++ src/Renderer.lua
@@ -23,6 +23,8 @@ function Renderer:init()
   self.DEFAULT_COLOR = {255,255,255}
   self.TEXT_Y_OFFSET = 2 * self.GU.FONT_SIZE
   self.TEXT_COLOR = {80, 80, 200}
+  
+  self.DRAW_ORDERING = {"Player Bullet", "Sinibomb", "Player"}
 end
 
 function Renderer:draw(xWorld)
@@ -30,16 +32,15 @@ function Renderer:draw(xWorld)
   local playerAngle = self.GU:getAngle(xWorld.player.dir)
   local movementJoystickMinR = PlayerInputParams.movementJoystick.minR
   local projectiles = xWorld.projectiles
+  local inverseCameraScale = 1/self.camera.scale
   
   self.captureDevice.loc = xWorld.player.loc
   self.captureDevice.inView = xWorld.collider:getCollisions(self.captureDevice)
-  -- can sort self.captureDevice.inView by object "types" (EX: can insert into layers by type)
+  local inViewByType = self:getObjectsInViewByType()
   
   -- ALWAYS LOOK AT THE PLAYER
   self.camera:lookAt(playerX, playerY)
-  self.camera:attach()
-  
-  local inverseCameraScale = 1/self.camera.scale
+  self.camera:attach()  
   
   -- THE ORIGIN
   love.graphics.setColor(255, 0, 0)
@@ -49,27 +50,41 @@ function Renderer:draw(xWorld)
     self.GU:centeredText("ORIGIN", 0, self.TEXT_Y_OFFSET)
   self.GU:END()
   
-  for i = 1, #self.captureDevice.inView do
-    local obj = self.captureDevice.inView[i]
-    local objRender = obj.render
-    local image = objRender.image
-    if image then
-      local color = objRender.color or self.DEFAULT_COLOR
-      love.graphics.setColor(unpack(color))
-      local angle = 0
-      if objRender.shouldRotate then
-        angle = self.GU:getAngle(obj.dir)
-      end   
-      self.GU:drawRotatedImage(image, obj.loc.x, obj.loc.y, angle)
-    end
-
-    love.graphics.setColor(self.TEXT_COLOR[1], self.TEXT_COLOR[2], self.TEXT_COLOR[3], self.TEXT_COLOR[4])
-    self.GU:BEGIN_SCALE(obj.loc, inverseCameraScale)
-      self.GU:centeredText(obj.type, obj.loc.x, obj.loc.y + self.TEXT_Y_OFFSET)
-    self.GU:END()
-  end
+  for i = 1, #self.DRAW_ORDERING do
+    local layerToDraw = self.DRAW_ORDERING[i]
+    local layerObjects = inViewByType[layerToDraw] or {}
     
+    for i = 1, #layerObjects do
+      local obj = layerObjects[i]
+      local objRender = obj.render
+      local image = objRender.image
+      if image then
+        local color = objRender.color or self.DEFAULT_COLOR
+        love.graphics.setColor(color[1], color[2], color[3], color[4])
+        local angle = 0
+        if objRender.shouldRotate then
+          angle = self.GU:getAngle(obj.dir)
+        end   
+        self.GU:drawRotatedImage(image, obj.loc.x, obj.loc.y, angle)
+      end
+  
+      love.graphics.setColor(self.TEXT_COLOR[1], self.TEXT_COLOR[2], self.TEXT_COLOR[3], self.TEXT_COLOR[4])
+      self.GU:BEGIN_SCALE(obj.loc, inverseCameraScale)
+        self.GU:centeredText(obj.type, obj.loc.x, obj.loc.y + self.TEXT_Y_OFFSET)
+      self.GU:END()
+    end
+  end
+
   self.camera:detach()  
 end
 
+function Renderer:getObjectsInViewByType()
+  local byType = {}
+  for k, obj in pairs(self.captureDevice.inView) do
+    local type = obj.type
+    if not byType[type] then byType[type] = {} end
+    table.insert(byType[type], obj)
+  end
+  return byType
+end
 return Renderer
