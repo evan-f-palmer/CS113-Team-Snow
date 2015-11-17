ac277879e79413a7e79ff5d63f9964ef07792caf
Dustin Shaffer <dustin.n.shaffer@gmail.com>
2015-11-16 22:07:50 -0800

    - Internal rename refactoring - Added "sinistar Crystals" and "number of crystals to build sinistar" fields and associated use case methods

--- src/GameData.lua
+++ src/GameData.lua
@@ -1,53 +1,67 @@
 local Class = require('hump.class')
 
-local PlayerGameData = Class{}
+local GameData = Class{}
 
-PlayerGameData.sinistarKillValue = 20000
-PlayerGameData.warriorKillValue = 500
-PlayerGameData.workerKillValue = 300
-PlayerGameData.crystalValue = 100
-PlayerGameData.asteroidKillValue = 50
+GameData.sinistarKillValue = 20000
+GameData.warriorKillValue = 500
+GameData.workerKillValue = 300
+GameData.crystalValue = 100
+GameData.asteroidKillValue = 50
 
-PlayerGameData.secondaryWeaponWarmupTime = 2
+GameData.secondaryWeaponWarmupTime = 2
 
-PlayerGameData.startingLives = 3
-PlayerGameData.startingHealth = 100
-PlayerGameData.damageFromCollisionWithSinistar = 1000
-PlayerGameData.damageFromCollisionWithWarriorBullet = 5
-PlayerGameData.damageFromCollisionWithWorkerBullet = 1
+GameData.startingLives = 3
+GameData.startingHealth = 100
+GameData.damageFromCollisionWithSinistar = 1000
+GameData.damageFromCollisionWithWarriorBullet = 5
+GameData.damageFromCollisionWithWorkerBullet = 1
 
-PlayerGameData.bombAmmoFromCrystalPickup = 1
+GameData.bombAmmoFromCrystalPickup = 1
 
+GameData.numberOfCrystalsToBuildSinistar = 35
 
-function PlayerGameData:init()
-  self.lives   = PlayerGameData.startingLives
+function GameData:init()
+  self.lives   = GameData.startingLives
   self.health  = 0
   self.score   = 0
   self.bombs   = 0
   self.alertMessage = ""
   self.alertPriority = 0
+  self.sinistarCrystals = 0
 end
 
-function PlayerGameData:updateAlertData(xAlertMachine)
+function GameData:updateAlertData(xAlertMachine)
   local primaryAlert = xAlertMachine:getPrimaryAlert()
   self.alertMessage = primaryAlert.message
   self.alertPriority = primaryAlert.priority
 end
 
-function PlayerGameData:isGameOver()
+function GameData:isGameOver()
   return self.lives <= 0
 end
 
-function PlayerGameData:increaseScore(xAmount)
+function GameData:increaseScore(xAmount)
   self.score = self.score + xAmount
 end
 
-function PlayerGameData:incrementLives()
+function GameData:incrementLives()
   self.lives = self.lives + 1
 end
 
-function PlayerGameData:decrementLives()
+function GameData:decrementLives()
   self.lives = self.lives - 1
 end
 
-return PlayerGameData
+function GameData:shouldSinistarBeCompleted()
+  return self.sinistarCrystals >= self.numberOfCrystalsToBuildSinistar
+end
+
+function GameData:incrementSinistarCrystals()
+  self.sinistarCrystals = self.sinistarCrystals + 1
+end
+
+function GameData:resetSinistarCrystals()
+  self.sinistarCrystals = 0
+end
+
+return GameData
