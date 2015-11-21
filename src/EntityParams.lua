094afac2c91c560b6b5761da9f06f0bf688230ac
Dustin Shaffer <dustin.n.shaffer@gmail.com>
2015-11-21 15:40:19 -0800

    - Tweak layouts and params

--- src/EntityParams.lua
+++ src/EntityParams.lua
@@ -2,7 +2,7 @@
 return {
   player = {
     health = 100,
-    maxSpeed = 1750,
+    maxSpeed = 1500,
     radius = 70,
     primaryFireOffset = 30,
     primaryFireDebounce = 0.1,
@@ -20,10 +20,10 @@ return {
     health = 40,
     radius = 70,
     sightRadius = 2000,
-    maxSpeedScale = 1,
+    maxSpeedScale = 0.9,
     maxForceScale = 1,
     fireDebounce = 5.6,
-    closestProximity = 350,
+    closestProximity = 400,
     damageFrom = {
       playerBullet = 10,
       sinibomb = 10,
@@ -34,18 +34,18 @@ return {
     radius = 70,
     primaryFireOffset = 20,
     sightRadius = 2000,
-    maxSpeedScale = 1,
+    maxSpeedScale = 0.5,
     maxForceScale = 1,
     maxDistanceFromFlock = 1000,
     fireDebounce = 3,
-    closestProximity = 700,
+    closestProximity = 800,
     damageFrom = {
       playerBullet = 10,
       sinibomb = 10,
     }
   },
   playerBullet = {
-    speed = 3200, lifespan = 3, radius = 4,
+    speed = 2200, lifespan = 3, radius = 4,
   },
   sinibomb = {
     speed = 100, lifespan = 5, radius = 25,
@@ -54,7 +54,7 @@ return {
     speed = 1200, lifespan = 2, radius = 5,
   },
   warriorBullet = {
-    speed = 2200, lifespan = 3, radius = 5,
+    speed = 1800, lifespan = 3, radius = 5,
   },
   crystal = {
     speed = 180, lifespan = 8, radius = 30,
--- src/InputDeviceLayout.lua
+++ src/InputDeviceLayout.lua
@@ -10,7 +10,7 @@ return {
   movementJoystick = {
     x = width/2,
     y = height/2,
-    minR = 75,
+    minR = 95,
     maxR = math.huge,
   },
 }
