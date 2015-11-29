5c13dc271e27ec242b80ea01ce19e79dccff48cb
dustin <misterdustinface@gmail.com>
2015-11-29 12:45:50 -0800

    - Code cleaning - Removed extra blue coloring from crystals

--- src/ProjectileDefinitions.lua
+++ src/ProjectileDefinitions.lua
@@ -23,9 +23,50 @@ local sinibombBlastAnimation = animator:newAnimation("Sinibomb Explosion", 3)
 sinibombBlastAnimation.start()
   
 local projectiles = Projectiles()
-projectiles:define("Player Bullet", {shouldRotate = true, image = love.graphics.newImage("assets/projectiles/projectile1.png"), color = {0,180,50}, speed = EntityParams.playerBullet.speed, lifespan = EntityParams.playerBullet.lifespan, radius = EntityParams.playerBullet.radius})
-projectiles:define("Sinibomb", {shouldRotate = true, image = love.graphics.newImage("assets/projectiles/projectile4.png"), color = {255,255,255}, speed = EntityParams.sinibomb.speed, lifespan = EntityParams.sinibomb.lifespan, radius = EntityParams.sinibomb.radius, onDeath = sinibombDeath, onCollision = sinibombCollision})
-projectiles:define("Sinibomb Blast", {shouldRotate = false, animation = sinibombBlastAnimation, color = {255,255,255}, speed = EntityParams.sinibombBlast.speed, lifespan = EntityParams.sinibombBlast.lifespan, radius = EntityParams.sinibombBlast.radius})
-projectiles:define("Worker Bullet", {shouldRotate = true, image = love.graphics.newImage("assets/projectiles/projectile3.png"), color = {195,145,40}, speed = EntityParams.workerBullet.speed, lifespan = EntityParams.workerBullet.lifespan, radius = EntityParams.workerBullet.radius}) 
-projectiles:define("Warrior Bullet", {shouldRotate = true, image = love.graphics.newImage("assets/projectiles/projectile6.png"), color = {200,100,0}, speed = EntityParams.warriorBullet.speed, lifespan = EntityParams.warriorBullet.lifespan, radius = EntityParams.warriorBullet.radius})
-projectiles:define("Crystal", {shouldRotate = true, image = love.graphics.newImage("assets/crystal.png"), color = {50,120,220}, speed = EntityParams.crystal.speed, lifespan = EntityParams.crystal.lifespan, radius = EntityParams.crystal.radius}) 
+projectiles:define("Player Bullet", {
+  shouldRotate = true, 
+  image = love.graphics.newImage("assets/projectiles/projectile1.png"), 
+  color = {0,180,50}, 
+  speed = EntityParams.playerBullet.speed, 
+  lifespan = EntityParams.playerBullet.lifespan, 
+  radius = EntityParams.playerBullet.radius
+})
+projectiles:define("Sinibomb", {
+  shouldRotate = true, 
+  image = love.graphics.newImage("assets/projectiles/projectile4.png"), 
+  speed = EntityParams.sinibomb.speed, 
+  lifespan = EntityParams.sinibomb.lifespan, 
+  radius = EntityParams.sinibomb.radius, 
+  onDeath = sinibombDeath, 
+  onCollision = sinibombCollision
+})
+projectiles:define("Sinibomb Blast", {
+  shouldRotate = false, 
+  animation = sinibombBlastAnimation, 
+  speed = EntityParams.sinibombBlast.speed, 
+  lifespan = EntityParams.sinibombBlast.lifespan, 
+  radius = EntityParams.sinibombBlast.radius
+})
+projectiles:define("Worker Bullet", {
+  shouldRotate = true, 
+  image = love.graphics.newImage("assets/projectiles/projectile3.png"), 
+  color = {195,145,40}, 
+  speed = EntityParams.workerBullet.speed, 
+  lifespan = EntityParams.workerBullet.lifespan, 
+  radius = EntityParams.workerBullet.radius
+}) 
+projectiles:define("Warrior Bullet", {
+  shouldRotate = true, 
+  image = love.graphics.newImage("assets/projectiles/projectile6.png"), 
+  color = {200,100,0}, 
+  speed = EntityParams.warriorBullet.speed, 
+  lifespan = EntityParams.warriorBullet.lifespan, 
+  radius = EntityParams.warriorBullet.radius
+})
+projectiles:define("Crystal", {
+  shouldRotate = true, 
+  image = love.graphics.newImage("assets/crystal.png"), 
+  speed = EntityParams.crystal.speed, 
+  lifespan = EntityParams.crystal.lifespan, 
+  radius = EntityParams.crystal.radius
+}) 
